0783128bb864ec19dfb778fff317f5c157cf92c0
Miguel Fajardo <miggy.fajardo13@gmail.com>
2016-12-18 18:54:25 -0700

    Add files via upload

--- sitemap.js
+++ sitemap.js
@@ -1,14 +1,15 @@
 function buildMap(sitemapWrapperid){
-    var siteMap = document.getElementById(sitemapWrapperid);
+    var area = document.getElementById(sitemapWrapperid);
     var list = document.createElement("ul");
     var subList = document.createElement("ul");
-  //  var links = document.creatElement("li");
-  //  links.textContent = "Links";
+    var drop = document.createElement("li");
+    drop.textContent = "Links";
     list.appendChild(item("Home", "index.html"));
     list.appendChild(item("Gallery", "gallery.html"));
-    list.appendChild(item("About", "about.html"));
+    list.appendChild(item("About", "about.html")); 
+	
 
-    
+	
     subList.appendChild(item("Breakout", "breakout.html"));
     subList.appendChild(item("Paint Splatter", "paint.html"));
     subList.appendChild(item("Eureka", "popup.html"));
@@ -18,16 +19,16 @@ function buildMap(sitemapWrapperid){
     subList.appendChild(item("Tic-Tac-Toe", "tictactoe.html"));
     subList.appendChild(item("Table", "table.html"));
     subList.appendChild(item("Multiplication Slider", "slider.html"));
-//    links.appendChild(subList);
-    list.appendChild(subList);
-    siteMap.appendChild(list);
+    drop.appendChild(subList);
+    list.appendChild(drop);
+    area.appendChild(list);
 }
 function item(name, link) {
-    var sitemapWrapper = document.createElement("a");
+    var sitemap = document.createElement("a");
     var li = document.createElement("li");
-    sitemapWrapper.href = link;
-    sitemapWrapper.textContent = name;
-    li.appendChild(sitemapWrapper);
+    sitemap.href = link;
+    sitemap.textContent = name;
+    li.appendChild(sitemap);
     return li;
 }
 
